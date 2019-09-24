5264308c7f10c473f52d90f811e18f2cad861684
Jimmy Saavedra <jimmy.saavedra@Beslogic.com>
2019-09-24 17:43:35 -0400

    Added dashboard component, general modifies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MesssagesComponent } from './messsages/messsages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MesssagesComponent
+    MesssagesComponent,
+    DashboardComponent
   ],
   imports: [
     AppRoutingModule,
