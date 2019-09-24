a94b7863e975e7bf49f800f90797cc0652814251
Jimmy Saavedra <jimmy.saavedra@Beslogic.com>
2019-09-24 13:47:20 -0400

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+
+  // hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  // On Select method
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+  ngOnInit() {
+  }
+
+}
